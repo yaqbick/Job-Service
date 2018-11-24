ed956955f7c172b245f3ee943c402a16a3d671cd
yaqbick <yaqbick@gmail.com>
2018-11-24 22:39:48 +0100

    displaying images

--- resources/views/employers/pics.blade.php
+++ resources/views/employers/pics.blade.php
@@ -6,7 +6,7 @@
   <?php $id=0; for($i=0; $i<$rowsCount;$i++) 
   {
     echo "<div class='row pt-5'>";
-    for($j=0;$j<2;$j++)
+    for($j=0;$j<4;$j++)
     {
  echo '<div class="col"><a href = "/employers/choose/imageId='.$images[$id]->id.'"><img src="'.$images[$id]->url.'" class="img-fluid rounded" width="100" height="100" ></div>';
       $id++;
